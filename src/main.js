36b969d7ce88521e6b0e2104836a129015b7e257
徐子为 <xu_ziwei@hoperun.com>
2020-01-10 11:37:02 +0800

    底部按钮适配全面屏

--- src/main.js
+++ src/main.js
@@ -8,3 +8,16 @@ App.mpType = 'app'
 
 const app = new Vue(App)
 app.$mount()
+Vue.prototype.globalData = getApp().globalData
+// 设置全面屏判断变量
+Vue.prototype.globalData.isFullScreen = false
+// 判断是否全面屏
+wx.getSystemInfo({
+  success: function (res) {
+    console.log(res)
+    // 根据 屏幕高度 进行判断
+    if (res.screenHeight - res.windowHeight - res.statusBarHeight - 32 > 72) {
+      Vue.prototype.globalData.isFullScreen = true
+    }
+  }
+})
--- src/pages/news/news-detail/index.vue
+++ src/pages/news/news-detail/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="view-news-detail">
+  <div :class="isFullScreen ? 'view-news-detail screen-full' : 'view-news-detail'">
     <div class="news-detail-title">{{news.title}}</div>
     <div class="news-detail-writer">{{news.writer}} {{news.time}}</div>
     <div class="news-detail-content">{{news.content}}</div>
 
-    <div class="weui-grids news-footer">
+    <div :class="isFullScreen ? 'weui-grids news-footer screen-full' : 'weui-grids news-footer'">
       <div class="weui-grid">
         <img src="../../../../static/images/news-detail/icon_share.png" alt="">
         <div>转发</div>
@@ -23,8 +23,6 @@
 
 <script>
 export default {
-  components: {
-  },
   data () {
     return {
       news: {
@@ -32,7 +30,8 @@ export default {
         content: '为支持实体经济发展，降低社会融资实际成本，中国人民银行决定于2020年1月6日下调金融机构存款准备金率0.5个百分点（不含财务公司、金融租赁公司和汽车金融公司）。中国人民银行将继续实施稳健的货币政策，保持灵活适度，不搞大水漫灌，兼顾内外平衡，保持流动性合理充裕。中国人民银行有关负责人表示：此次降准是全面降准，体现了逆周期调节，释放长期资金约8000多亿元。',
         writer: '润和资讯研究所',
         time: '2020-1-1'
-      }
+      },
+      isFullScreen: this.globalData.isFullScreen
     }
   }
 }
@@ -41,6 +40,10 @@ export default {
 <style lang="scss" scoped>
 .view-news-detail {
   padding: 10px 15px 70px;
+
+  &.screen-full {
+    padding-bottom: 130px !important;
+  }
 }
 .news-detail-title {
   font-size: 15px;
@@ -63,6 +66,8 @@ export default {
   text-align: center;
   border-top: 2px solid #bbbbbb;
   overflow: visible;
+  background: #fff;
+  height: 51px;
 
   .weui-grid {
     border: 0;
@@ -80,11 +85,16 @@ export default {
   .icon-large {
     width: 50px;
     height: 50px;
+    border-radius: 50px;
     margin-top: -32px;
   }
 
   .btn-create div {
     margin-top: 6px;
+  }
+
+  &.screen-full {
+    height: 85px !important;
   }
 }
 </style>
